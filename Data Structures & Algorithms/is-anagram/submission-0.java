113bdb00c312d65583c233941da0133766a217a9
rkpandeykn-sudo <rkpandeykn@gmail.com>
2026-07-14 10:16:20 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    //Sorting 
+    public boolean isAnagram(String s, String t) {
+        char[] sc = s.toCharArray();
+        char[] tc = t.toCharArray();
+
+        Arrays.sort(sc);
+        Arrays.sort(tc);
+
+        return new String(sc).equals(new String(tc));
+    }
+}
